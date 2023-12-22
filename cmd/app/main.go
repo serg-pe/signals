45285b9437adfa3dd66676371da7ca3703186555
Serg <pechuirn.serg@gmail.com>
2023-12-23 02:51:43 +0300

    rewrite server & graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"sync"
+	"syscall"
 
 	"github.com/serg-pe/signals/internal/config"
 	"github.com/serg-pe/signals/internal/server"
@@ -50,6 +54,16 @@ func createConfigFileAndExit(path string) {
 }
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	defer func() {
+		fmt.Println("shutdown signal")
+		cancel()
+	}()
+
 	cfg := readConfigFile()
 
 	logger, err := logger.New(cfg.LoggerConfig)
@@ -63,18 +77,25 @@ func main() {
 	}
 
 	wg := sync.WaitGroup{}
-	wg.Add(1)
 
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
-		err := server.Run()
-		if err != nil {
-			logger.Fatal("server stoped", zap.Error(err))
+		err := server.Run(ctx)
+		if err == http.ErrServerClosed {
+			logger.Info("server stoped")
+		} else {
+			logger.Error("server stoped with error", zap.Error(err))
 		}
 	}()
 	logger.Info("server successfully started", zap.String("ip", cfg.Ip), zap.Uint16("port", cfg.Port))
 
-	wg.Wait()
+	go func() {
+		<-sig
+		logger.Info("stop server")
+		server.Stop(ctx)
+	}()
 
+	wg.Wait()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,41 +1,165 @@
 package server
 
 import (
+	"context"
+	"encoding/hex"
 	"fmt"
+	"net"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/serg-pe/signals/internal/config"
-	"github.com/serg-pe/signals/internal/server/handlers"
 	"go.uber.org/zap"
 )
 
+const (
+	queryIsPublisherName = "is-initiator"
+)
+
 type Server struct {
-	logger   *zap.Logger
-	cfg      config.ServerConfig
-	serveMux *http.ServeMux
+	logger *zap.Logger
+	cfg    config.ServerConfig
+
+	server *http.Server
+
+	upgrader websocket.Upgrader
+
+	wg    *sync.WaitGroup
+	pubMu *sync.Mutex
+	pub   *websocket.Conn
+	subMu *sync.Mutex
+	sub   *websocket.Conn
 }
 
 func New(cfg config.ServerConfig, logger *zap.Logger) (Server, error) {
 	server := Server{
-		logger:   logger,
-		cfg:      cfg,
-		serveMux: http.NewServeMux(),
-	}
+		logger: logger.Named("server"),
+		cfg:    cfg,
 
-	server.setupRoutes()
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferPool: &sync.Pool{},
+			CheckOrigin:     func(r *http.Request) bool { return true },
+		},
+
+		wg:    &sync.WaitGroup{},
+		pubMu: &sync.Mutex{},
+		subMu: &sync.Mutex{},
+	}
 
 	return server, nil
 }
 
-func (s *Server) setupRoutes() {
-	s.serveMux.Handle("/connection/", handlers.NewConnectionHandler(s.logger))
+func (s *Server) setupRoutes() *http.ServeMux {
+	// s.serveMux.Handle("/connection/", handlers.NewConnectionHandler(s.logger))
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/connection/", s.connect)
+
+	return mux
 }
 
-func (s *Server) Run() error {
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", s.cfg.Ip, s.cfg.Port), s.serveMux)
+func (s *Server) connect(w http.ResponseWriter, r *http.Request) {
+	var (
+		err   error
+		isPub bool
+	)
+
+	if !websocket.IsWebSocketUpgrade(r) {
+		s.logger.Debug("no upgrade protocol header", zap.String("from", r.RemoteAddr))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if r.URL.Query().Has(queryIsPublisherName) {
+		isPubRaw := r.URL.Query().Get(queryIsPublisherName)
+		isPub, err = strconv.ParseBool(isPubRaw)
+		if err != nil {
+			s.logger.Debug("parse bool error", zap.String("client", r.RemoteAddr), zap.String("value", isPubRaw))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		s.logger.Error("upgrade connection", zap.String("client", r.RemoteAddr), zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if !isPub {
+		s.sub = conn
+		s.logger.Info("subscriber connected", zap.String("address", r.RemoteAddr))
+		s.wg.Add(1)
+		go func() {
+			defer s.wg.Done()
+			for {
+				msgType, msg, err := s.sub.ReadMessage()
+				if err != nil && websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+					break
+				}
+				if err != nil {
+					s.logger.Error("read message", zap.Error(err))
+					break
+				}
+				s.logger.Debug("new message", zap.Int("type", msgType), zap.String("msg", hex.EncodeToString(msg)))
+			}
+			s.logger.Info("client disconnected")
+			s.sub = nil
+		}()
+	}
+
+	if s.pub != nil {
+
+	}
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	server := &http.Server{
+		Addr:         fmt.Sprintf("%s:%d", s.cfg.Ip, s.cfg.Port),
+		Handler:      s.setupRoutes(),
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	s.server = server
+
+	err := s.server.ListenAndServe()
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (s *Server) Stop(ctx context.Context) {
+	if s.sub != nil {
+		s.subMu.Lock()
+		defer s.subMu.Unlock()
+		err := s.sub.Close()
+		if err != nil {
+			s.logger.Debug("close connection error", zap.String("subscriber", s.sub.RemoteAddr().String()))
+		}
+	}
+	if s.pub != nil {
+		s.pubMu.Lock()
+		defer s.pubMu.Unlock()
+		err := s.pub.Close()
+		if err != nil {
+			s.logger.Debug("close connection error", zap.String("publisher", s.pub.RemoteAddr().String()))
+		}
+	}
+
+	if s.server != nil {
+		s.server.Shutdown(ctx)
+	}
+
+	s.wg.Wait()
 }
